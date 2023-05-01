c83cc4282b684ab7523c14e0132d1273da94ebb2
Mateus Cesar <mateuscesarglima@gmail.com>
2023-05-01 16:37:24 -0300

    feat(RENTX-3): Add alias config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
-import Home from '@Screens/Home';
+import { Home } from '@Screens/Home';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Home />
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
-import {
-   Container
-} from './styles';
+import { Container } from './styles';
 
-export const Home = () => {
+const Home = () => {
   return (
-   <Container>
+    <Container>
       <Text>Testando</Text>
-   </Container>
+    </Container>
   );
-}
+};
+
+export default Home;
